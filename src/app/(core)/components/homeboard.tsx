75377122c8fc2b8c74348c70420b116290f69121
Julien Boz <julien.boz@mobi.ch>
2023-08-19 17:36:38 +0200

    feat: ✨ home hit indicator + mobile display

--- src/app/(core)/components/homeboard.tsx
+++ src/app/(core)/components/homeboard.tsx
@@ -1,35 +1,29 @@
 "use client"
 
 import { useState } from 'react';
+import { BoardSquareProps, Ship, ShipProps, Square } from '../model';
 import Board from './board';
 import styles from './page.module.css';
 
 interface HomeBoardProps {
   started: boolean;
+  ships: Ship[];
+  setShips: (ships: Ship[]) => any;
 }
 
-export default function HomeBoard({started}: HomeBoardProps) {
+export default function HomeBoard({started, ships, setShips}: HomeBoardProps) {
   const dimensions = Array.from({ length: 10 }, (_, i) => i + 1);
-  
-  const [ships, setShips] = useState<Ship[]>([
-    { id: 1, name: 'Carrier', size: 5, color: '#be456e' },
-    { id: 2, name: 'Battleship', size: 4, color: '#0494ff' },
-    // { id: 3, name: 'Battleship', size: 4, color: '#0494ff' },
-    { id: 4, name: 'Cruiser', size: 3, color: '#04ff8a' },
-    // { id: 5, name: 'Cruiser', size: 3, color: '#04ff8a' },
-    { id: 6, name: 'Submarine', size: 3, color: '#04ffde' },
-    // { id: 7, name: 'Submarine', size: 3, color: '#04ffde' },
-    { id: 8, name: 'Destroyer', size: 2, color: '#af54a0' },
-  ]);
-  
-  const newSquare = (x: number, y: number) => ({ coords: { x, y }, hasShip: false, isHit: false } as Square)
-  const squareKey = (square: Square) => `${square.coords.x}:${square.coords.y}`;
-  
+
   const [placementMode, setPlacementMode] = useState<'vertical' | 'horizontal'>('horizontal');
   const [squares, setSquares] = useState<Square[]>(dimensions.map((x) => dimensions.map((y) => newSquare(x, y))).reduce((a, b) => [...a, ...b], []));
   const [selectedShip, setSelectedShip] = useState<Ship | undefined>(undefined);
 
   const squareOnClick = (clickedSquare: Square) => {
+    if (started) {
+      clickedSquare.isHit = !clickedSquare.isHit;
+      setSquares([...squares]);
+      return;
+    }
     if (clickedSquare.ship) {
       // remove ship from the board and select it
       clickedSquare.ship.placed = false;
@@ -37,7 +31,6 @@ export default function HomeBoard({started}: HomeBoardProps) {
       setSelectedShip(clickedSquare.ship);
       squares.filter(square => square.ship?.id === clickedSquare.ship?.id).forEach(square => square.ship = undefined);
       setSquares([...squares]);
-      togglePlacementMode();
 
     } else if (selectedShip) {
       // place the ship if possible
@@ -121,7 +114,7 @@ export default function HomeBoard({started}: HomeBoardProps) {
         </Board>
 
         {!started && (<div className={styles.ships}>
-          {ships.map((ship, index) => <ShipComponent key={'ship' + index} ship={ship} selectable={!ship.placed} onSelection={setSelectedShip} selected={selectedShip?.id === ship.id} />)}
+          {ships.filter(s => !s.placed).map((ship, index) => <BoardShip key={'ship' + index} ship={ship} onSelection={setSelectedShip} selected={selectedShip?.id === ship.id} />)}
         </div>)}
       </div>
     </div>
@@ -129,47 +122,19 @@ export default function HomeBoard({started}: HomeBoardProps) {
 }
 
 const BoardSquare = ({ square, onClick, onMouseEnter, onMouseOut }: BoardSquareProps) => {
-
-  return <div className={styles.cell} style={{ backgroundColor: square.ship?.color || square.hoverColor || '#e6e2f1' }} onClick={() => onClick(square)} onMouseEnter={() => onMouseEnter(square)} onMouseOut={() => onMouseOut(square)}></div>
+  return <div 
+      className={styles.cell} 
+      style={{ backgroundColor: square.ship && square.isHit ? '#65000b' : square.ship?.color || square.hoverColor || '#e6e2f1' }} 
+      onClick={() => onClick(square)} 
+      onMouseEnter={() => onMouseEnter(square)} 
+      onMouseOut={() => onMouseOut(square)}></div>
 }
 
-const ShipComponent = ({ ship, onSelection, selected }: ShipProps) => {
-
+const BoardShip = ({ ship, onSelection, selected }: ShipProps) => {
   return <div className={styles.ship} onClick={() => !ship.placed && onSelection(ship)}>
     {Array.from({ length: ship.size }).map((_, index) => (<div key={ship.name + index} className={styles.cell} style={{ backgroundColor: !ship.placed ? ship.color : '#84808b', borderColor: selected ? '#e6e2f1' : '#272138' }}></div>))}
   </div>
 }
-
-interface ShipProps {
-  ship: Ship;
-  selectable: boolean;
-  onSelection: (ship: Ship) => any;
-  selected: boolean;
-}
-
-interface BoardSquareProps {
-  square: Square;
-  onClick: (square: Square) => any;
-  onMouseEnter: (square: Square) => any;
-  onMouseOut: (square: Square) => any;
-}
-
-interface Coordinates {
-  x: number;
-  y: number;
-}
-
-interface Square {
-  ship?: Ship;
-  hoverColor?: string;
-  isHit: boolean;
-  coords: Coordinates;
-}
-
-interface Ship {
-  id: number;
-  name: string;
-  size: number;
-  color: string;
-  placed?: boolean;
-}
+  
+const newSquare = (x: number, y: number) => ({ coords: { x, y }, hasShip: false, isHit: false } as Square)
+const squareKey = (square: Square) => `${square.coords.x}:${square.coords.y}`;
